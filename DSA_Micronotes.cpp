b2007140d0275b21c87f15dd7c606477c1ba322c
rakesh2022 <83332290+rakesh2022@users.noreply.github.com>
2022-09-25 23:55:03 +0530

    Added Traveling_salesman_prob

--- DSA_Micronotes.cpp
+++ DSA_Micronotes.cpp
@@ -1,5 +1,12 @@
 GRAPH:
-
-  a)  The Bellman–Ford algorithm is an algorithm that computes shortest paths from a single source vertex to all of the other vertices in a weighted digraph whereas Floyd-Warshall computes shortest paths from each node to every other node.
-   b)  Note: whenever question says pre-requisite or to complete some task before some task use topological sort
-    link: https://www.geeksforgeeks.org/minimum-time-taken-by-each-job-to-be-completed-given-by-a-directed-acyclic-graph/
+   1) a)  The Bellman–Ford algorithm is an algorithm that computes shortest paths from a single source vertex to all of the other vertices in a weighted digraph whereas Floyd-Warshall computes shortest paths from each node to every other node.
+      b)  Note: whenever question says pre-requisite or to complete some task before some task use topological sort
+        link: https://www.geeksforgeeks.org/minimum-time-taken-by-each-job-to-be-completed-given-by-a-directed-acyclic-graph/
+    2) a) Traveling SalesMan Problem uses below concept
+          The Hamiltoninan cycle problem is to find if there exist a tour that visits
+          every city exactly once. Here we know that Hamiltonian Tour exists (because
+          the graph is complete) and in fact many such tours exist, the problem is to
+          find a minimum weight Hamiltonian Cycle.
+          problem:
+          given cities and dist. between each pair of cities,
+          find the shortest possible path that visits each cities and return to origin city
